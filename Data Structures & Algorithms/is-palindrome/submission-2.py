915088fce90add74d4c3be75fd3ad0eb5f363650
Ohayoune <72_tongs_sacred@icloud.com>
2026-07-23 18:35:26 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start = 0
+        end = len(s)-1
+        s = s.lower()
+        while start <= end:
+            sVal = ord(s[start])
+            if not (97 <=sVal<=122 or 48 <=sVal <= 57):
+                start+=1
+                continue
+            eVal = ord(s[end])
+            if not (97 <= eVal <= 122 or 48 <= eVal <=57):
+                end -= 1
+                continue
+            print(f"start: {sVal}, end: {eVal}")
+            print(f"{start}, {end}")
+            if eVal != sVal:
+                return False
+            else:
+                start +=1
+                end -= 1
+        
+
+
+
+
+
+        return True
+        
